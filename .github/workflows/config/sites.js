8176ebf5a0e5832b9d0ae9f4b3ca1b854c4c17dd
pariuverdero-byte <pariuverdero@gmail.com>
2026-01-18 17:39:49 +0200

    Create sites.js

---
+++ .github/workflows/config/sites.js
@@ -0,0 +1,14 @@
+module.exports = {
+  pariuverde: {
+    siteUrl: "https://pariuverde.ro",
+    wpApi: "https://pariuverde.ro/wp-json/wp/v2",
+    language: "ro",
+    category: 123, // categoria RO
+  },
+  greenbettips: {
+    siteUrl: "https://greenbettips.com",
+    wpApi: "https://greenbettips.com/wp-json/wp/v2",
+    language: "en",
+    category: 456, // categoria EN
+  }
+};
