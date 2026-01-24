97482870092d701c7e8d528d9b70fff548bfcfee
pariuverdero-byte <pariuverdero@gmail.com>
2026-01-24 16:29:52 +0200

    Create 10pariuri_sources.js

---
+++ parsers/10pariuri_sources.js
@@ -0,0 +1,32 @@
+export const SOURCES_10PARIURI = [
+  {
+    key: "tenis_azi",
+    url: "https://10pariuri.ro/ponturi-pariuri-azi/tenis-24-01-2026/",
+    sport: "tennis",
+  },
+  {
+    key: "cota2",
+    url: "https://10pariuri.ro/biletul-zilei-la-pariuri/cota-2-24012026/",
+    sport: "football",
+  },
+  {
+    key: "cota3",
+    url: "https://10pariuri.ro/biletul-zilei-la-pariuri/fotbal-cota-3-24012026/",
+    sport: "football",
+  },
+  {
+    key: "cota_mare",
+    url: "https://10pariuri.ro/biletul-zilei-la-pariuri/cota-mare-24012026/",
+    sport: "football",
+  },
+  {
+    key: "ban_pe_ban",
+    url: "https://10pariuri.ro/biletul-zilei-la-pariuri/bilet-fotbal-ban-pe-ban-ion-dan-24012026/",
+    sport: "football",
+  },
+  {
+    key: "pontul_zilei",
+    url: "https://10pariuri.ro/pontul-zilei/pontul-zilei-24012026/",
+    sport: "mixed",
+  },
+];
