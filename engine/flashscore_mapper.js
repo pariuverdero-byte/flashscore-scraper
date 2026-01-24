c0e3f0279ba126be3df3c7c3595366c3841c8270
pariuverdero-byte <pariuverdero@gmail.com>
2026-01-24 16:45:03 +0200

    Create flashscore_mapper.js

---
+++ engine/flashscore_mapper.js
@@ -0,0 +1,12 @@
+/**
+ * Fallback mapping using Flashscore
+ * This will be implemented later using API-Football / Flashscore scraper
+ */
+export async function flashscoreMapMatch({ sport, rawText }) {
+  // TODO:
+  // 1. detect league / competition
+  // 2. search in Flashscore live / upcoming
+  // 3. fuzzy match teams / players
+
+  return null;
+}
