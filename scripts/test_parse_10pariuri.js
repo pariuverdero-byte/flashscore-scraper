9ab8deba4d0668f855a9436308794f5424c6884e
pariuverdero-byte <pariuverdero@gmail.com>
2026-01-24 16:16:33 +0200

    Update test_parse_10pariuri.js

--- scripts/test_parse_10pariuri.js
+++ scripts/test_parse_10pariuri.js
@@ -1,20 +1,18 @@
-// FIX pentru undici / axios
-global.File = class File {};
-
-import axios from "axios";
 import { parse10PariuriCota2 } from "../parsers/parse10pariuri_cota2.js";
 
 const URL =
   "https://10pariuri.ro/biletul-zilei-la-pariuri/cota-2-24012026/";
 
 async function run() {
-  const res = await axios.get(URL, {
+  const res = await fetch(URL, {
     headers: {
       "User-Agent": "Mozilla/5.0",
     },
   });
 
-  const parsed = parse10PariuriCota2(res.data, URL);
+  const html = await res.text();
+
+  const parsed = parse10PariuriCota2(html, URL);
 
   console.log("==== PARSE RESULT ====");
   console.log(JSON.stringify(parsed, null, 2));
